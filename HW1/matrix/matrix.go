61f4f89ae524b8e7091c6fd5d75dc31caf5c7917
Linh Huynh <128510005+LinhHuynh2403@users.noreply.github.com>
2025-01-24 14:37:36 -0800

    Update matrix.go

--- HW1/matrix/matrix.go
+++ HW1/matrix/matrix.go
@@ -1,84 +1,109 @@
 package matrix
 
 // If needed, you may define helper functions here.
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
 
 // AreAdjacent returns true iff a and b are adjacent in lst.
 func AreAdjacent(a, b int, lst []int) bool {
-	panic("TODO: implement this!")
-	// to check if a and b are adjacent, we check the index of a and b
-	// if (abs(index(a) - index(b) == 1)) then return true, otherwise, return false
-	var index_a, index_b = -1, -1
-
-	for i := 0; i < len(lst); i++ {
-		if lst[i] == a {
-			index_a = i
-		}
-		if lst[i] == b {
-			index_b = i
-		}
-	}
-	// when a and b are not in the matrix
-	if index_a == -1 || index_b == -1 {
+	// if the matrix length is 1 return false, since there is only one element
+	if len(lst) < 2 {
 		return false
 	}
-	return abs(index_a-index_b) == 1
+	/*
+		first find the element a is in the matrix
+		if a is in matrix, check the left and right index whether the element is b
+		if the left or the right is equal to b, return true
+	*/
+
+	for i, num := range lst {
+		if num == a {
+			// neighbor on the right side
+			if i > 0 && lst[i-1] == b {
+				return true
+			}
+			// neighbor on the left side
+			if i < len(lst)-1 && lst[i+1] == b {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // Transpose returns the transpose of the 2D matrix mat.
-func Transpose(mat [][]int) [][]int {
-	panic("TODO: implement this!")
-	// create a new transpose_matrix n x m matrix
-	var m, n = len(mat), len(mat[0])
 
-	// create new result matrix (slice)
-	transpose_matrix := make([][]int, n)
-	for i := range transpose_matrix {
-		transpose_matrix[i] = make([]int, m)
+func Transpose(mat [][]int) [][]int {
+	// Check if the input matrix is empty
+	if mat == nil {
+		return nil
+	}
+	if len(mat) == 0 || len(mat[0]) == 0 {
+		return [][]int{}
 	}
 
-	// loop through the origin array
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			transpose_matrix[j][i] = mat[i][j]
+	row := len(mat)    // Number of rows in the input matrix
+	col := len(mat[0]) // Number of columns in the input matrix
+
+	// Create a new 2D slice for the transposed matrix
+	tranposeMatrix := make([][]int, col)
+	for i := range tranposeMatrix {
+		tranposeMatrix[i] = make([]int, row) // Each row in the transpose_matrix has 'row' elements
+		for j := range tranposeMatrix[i] {
+			tranposeMatrix[i][j] = mat[j][i] // Swap rows and columns
 		}
 	}
 
-	return transpose_matrix
+	return tranposeMatrix
 }
 
-// AreNeighbors returns true iff a and b are neighbors in the 2D matrix mat.
+// find neighbor: a and b are neighbors iff they are next to each other in vertica or horizontal
 func AreNeighbors(mat [][]int, a, b int) bool {
-	panic("TODO: implement this!")
-	// a and b are neighbors iff they are next to each other in vertica or horizontal
-	// iterate through matrix and find location of a and b
-	// a(i1, j1) and b(i2, j2)
-	var i1, j1, i2, j2 int
-	var m, n = len(mat), len(mat[0])
+	// check the length of matrix if it is empty
+	if len(mat) == 0 || len(mat[0]) == 0 {
+		return false
+	}
+	// do the same as AreNeighbor in the first part but this time is m x n
+	// find element a, and check if left, right, up, or down is element b
+	// if yes, return true, otherwise, return false
 
-	i1, i2, j1, j2 = -1, -1, -1, -1 // when a and b are not in the matrix
+	posA := [2]int{} // store location of A
+	foundA := false
 
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
+	// find a
+	for i := range mat {
+		for j := range mat[0] {
 			if mat[i][j] == a {
-				i1 = i
-				j1 = j
+				posA[0] = i
+				posA[1] = j
+				foundA = true
+				break
 			}
-			if mat[i][j] == b {
-				i2 = i
-				j2 = j
+		}
+		if foundA {
+			break
+		}
+	}
+
+	// If `a` was not found, return false
+	if !foundA {
+		return false
+	}
+
+	// Check the positions left, right, up, and down of `a`
+	directions := [][2]int{
+		{-1, 0}, // Up
+		{1, 0},  // Down
+		{0, -1}, // Left
+		{0, 1},  // Right
+	}
+
+	for _, dir := range directions {
+		newRow, newCol := posA[0]+dir[0], posA[1]+dir[1]
+		if newRow >= 0 && newRow < len(mat) && newCol >= 0 && newCol < len(mat[0]) { // Ensure within bounds
+			if mat[newRow][newCol] == b {
+				return true
 			}
 		}
 	}
 
-	if ((abs(i1-i2) == 1) && j1 == j2) || ((abs(j1-j2) == 1) && i1 == i2) {
-		return true
-	}
 	return false
-
 }
