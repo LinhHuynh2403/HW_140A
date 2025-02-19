e1247a61ab46167c392f5741bbe86f46b300cfc7
ptqnhan <ptqnhan@gmail.com>
2025-02-19 14:50:21 -0800

    ...

--- HW3/hw3-handout-solved/lgraph/lgraph.lisp
+++ HW3/hw3-handout-solved/lgraph/lgraph.lisp
@@ -1,9 +1,11 @@
 ;; You may define helper functions here
 
 (defun find-sequence (g1 g2 start target k)
-    (cond
-        ((and (equal (g1 start) nil) ((equal (g2 start) nil ))) ())
-    )
+
 )
 
-(defun )
+(defun dfs (g start target k path)
+    (cond
+        (and (= k 0) (
+    )
+) 
