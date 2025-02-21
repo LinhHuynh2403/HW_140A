6f6f85cb6e1d902851ffd5332d41e54e9280d519
ptqnhan <ptqnhan@gmail.com>
2025-02-21 05:41:24 -0800

    match using pure lisp

--- HW3/hw3-handout-solved/match/match.lisp
+++ HW3/hw3-handout-solved/match/match.lisp
@@ -3,5 +3,25 @@
 (defun match (pattern assertion)
   ;; TODO: incomplete function. 
   ;; The next line should not be in your solution.
-  (list 'incomplete)
-)
+  ;; (list 'incomplete)
+  (cond
+    ;; Check for exact match when no special atoms are present
+    ((null pattern) (null assertion))
+
+    ;; Handling pattern containing ?
+    ((equal (car pattern) '?)
+     (and (not (null assertion))
+          (match (cdr pattern) (cdr assertion))))
+
+    ;; Handling pattern containing !
+    ((equal (car pattern) '!)
+     (or (match (cdr pattern) (cdr assertion))
+         (and (not (null assertion))
+              (match pattern (cdr assertion)))))
+
+    ;; Matching corresponding elements
+    ((equal (car pattern) (car assertion))
+     (match (cdr pattern) (cdr assertion)))
+
+    ;; No match
+    (t nil)))
