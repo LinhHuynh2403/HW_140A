f5b9f7bafc88d206924c95ed4d74ebdd1b6662a5
Linh Huynh <128510005+LinhHuynh2403@users.noreply.github.com>
2025-02-21 21:25:37 -0800

    Update match.lisp
    Fixing by not using loop

--- hw3-handout(1)/match/match.lisp
+++ hw3-handout(1)/match/match.lisp
@@ -1,14 +1,15 @@
 (defun match (pattern assertion)
-  (cond
-    ((and (null pattern) (null assertion)) t)  ; Both empty: match
-    ((or (null pattern) (null assertion)) nil) ; One empty: no match
-    ((equal (car pattern) '?)                  ; Handle '?'
-     (and (consp assertion)                    ; Assertion has at least one element
-          (match (cdr pattern) (cdr assertion))))
-    ((equal (car pattern) '!)                  ; Handle '!'
-     (loop for i from 1 to (length assertion)  ; Try all possible splits
-           thereis (match (cdr pattern) (nthcdr i assertion))))
-    (t                                         ; Regular atom comparison
-     (and (equal (car pattern) (car assertion))
-          (match (cdr pattern) (cdr assertion)))))
-)
+(cond
+  ((and (null pattern) (null assertion)) t)  ; Both empty: match
+  ((or (null pattern) (null assertion)) nil) ; One empty: no match
+  ((equal (car pattern) '?)                  ; Handle '?'
+    (and (consp assertion)                    ; Assertion has at least one element
+        (match (cdr pattern) (cdr assertion))))
+  ((equal (car pattern) '!)                  ; Handle '!'
+    (or (and (consp assertion) 
+            (match (cdr pattern) (cdr assertion)))  ; Consume one element
+        (and (consp assertion) 
+            (match pattern (cdr assertion)))))      ; Try skipping one element
+  (t                                         ; Regular atom comparison
+    (and (equal (car pattern) (car assertion))
+        (match (cdr pattern) (cdr assertion))))))
