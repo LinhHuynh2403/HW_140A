91c14ec96fc2b82acdc9d441680d37f6a5c0c7be
Nguyen Tran <137568588+nguyjtra@users.noreply.github.com>
2025-03-06 18:54:02 -0800

    Add files via upload

--- disjointset.go
+++ disjointset.go
@@ -3,6 +3,7 @@ package disjointset
 // DisjointSet is the interface for the disjoint-set (or union-find) data
 // structure.
 // Do not change the definition of this interface.
+
 type DisjointSet interface {
 	// UnionSet(s, t) merges (unions) the sets containing s and t,
 	// and returns the representative of the resulting merged set.
@@ -11,48 +12,56 @@ type DisjointSet interface {
 	FindSet(int) int
 }
 
-type disjointSet struct {
-	parent map[int]int
-	size   map[int]int
+type DisjointSetImpl struct{
+	parent map[int]int 		// map from node -> parent
+	size map[int]int 		// map from node -> rank
+}
+
+
+func (ds *DisjointSetImpl) FindSet(s int) int {
+	if p, ok := ds.parent[s]; !ok {
+		ds.parent[s] = s
+		ds.size[s] = 1
+		return s 
+	} else if s != p {
+		r := ds.FindSet(p)
+		ds.parent[s] = r
+		return r
+	} else {
+		return s
+	}
+}	
+
+func (ds *DisjointSetImpl) UnionSet(s, t int) int {
+	s, t = ds.FindSet(s), ds.FindSet(t)
+	if s == t {
+		return s
+	}
+	sizeS, sizeT := ds.size[s], ds.size[t]
+	if sizeS < sizeT {
+		s, t = t, s
+	}
+	ds.parent[t] = s
+	ds.size[s] = sizeS + sizeT
+	return s
 }
 
 func NewDisjointSet() DisjointSet {
-	return &disjointSet{
-		parent: make(map[int]int),
-		size:   make(map[int]int),
-	}
+	return &DisjointSetImpl{make(map[int]int), make(map[int]int)}
 }
 
-func (d *disjointSet) FindSet(u int) int {
-	if _, ok := d.parent[u]; !ok {
-		d.parent[u] = u
-		d.size[u] = 1
-		return u
+func MergeToUnionSets(set1 DisjointSet, set2 DisjointSet) DisjointSet {
+	set_1 := set1.(*DisjointSetImpl)
+	set_2 := set2.(*DisjointSetImpl)
+	for key, val := range set_2.parent {
+		if _, ok := set_1.parent[key]; !ok {
+            set_1.parent[key] = val
+		}
 	}
-	// Path compression
-	for d.parent[u] != u {
-		d.parent[u] = d.parent[d.parent[u]]  // Path compression step
-		u = d.parent[u]
-	}
-	return u
+	return set_1
 }
 
-func (d *disjointSet) UnionSet(a, b int) int {
-	rootA := d.FindSet(a)
-	rootB := d.FindSet(b)
-
-	if rootA == rootB {
-		return rootA
-	}
-
-	// Union by size
-	if d.size[rootA] < d.size[rootB] {
-		d.parent[rootA] = rootB
-		d.size[rootB] += d.size[rootA]
-		return rootB
-	} else {
-		d.parent[rootB] = rootA
-		d.size[rootA] += d.size[rootB]
-		return rootA
-	}
+func GetParent(setDis DisjointSet) map[int]int {
+	set := setDis.(*DisjointSetImpl)
+	return set.parent
 }
