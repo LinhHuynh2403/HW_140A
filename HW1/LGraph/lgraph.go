44badc32d45afe8ee1f3c7a8aca3d6c267e3281a
ptqnhan <ptqnhan@gmail.com>
2025-03-17 11:26:57 -0700

    ...

--- HW1/LGraph/lgraph.go
+++ HW1/LGraph/lgraph.go
@@ -25,7 +25,6 @@ func FindSequence(g1, g2 LGraph, s node, t node, k uint) ([]rune, bool) {
 	return nil, false
 }
 
-// helper function
 func generateSequences(g LGraph, s node, t node, k uint) [][]rune {
 	var result [][]rune
 	var dfs func(current node, path []rune, steps uint)
@@ -39,13 +38,10 @@ func generateSequences(g LGraph, s node, t node, k uint) [][]rune {
 			}
 			return
 		}
-
 		edges, exists := g(current)
 		if !exists {
 			return
 		}
-
-		
 		for _, e := range edges {
 			dfs(e.destination, append(path, rune(e.label)), steps+1)
 		}
@@ -53,4 +49,37 @@ func generateSequences(g LGraph, s node, t node, k uint) [][]rune {
 
 	dfs(s, []rune{}, 0)
 	return result
+}
+
+func isSequencePresent(g LGraph, s node, t node, sequence []rune) bool {
+    if len(sequence) == 0 {
+        _, exists := g(s)
+        return exists && s == t
+    }
+
+    currentNodes := make(map[node]bool)
+    currentNodes[s] = true
+
+    for _, label := range sequence {
+        nextNodes := make(map[node]bool)
+
+        for n := range currentNodes {
+            edges, exists := g(n)
+            if !exists {
+                continue
+            }
+            for _, e := range edges {
+                if e.label == label {
+                    nextNodes[e.destination] = true
+                }
+            }
+        }
+
+        if len(nextNodes) == 0 {
+            return false
+        }
+        currentNodes = nextNodes
+    }
+
+    return currentNodes[t]
 }
