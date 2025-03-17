6ae9c09da0574dd7b6f9fa8de1c986a8971f1d1c
ptqnhan <ptqnhan@gmail.com>
2025-03-17 12:11:44 -0700

    changing lgraph.go concurrency

--- hw5/hw5-handout/bug1/bug1.go
+++ hw5/hw5-handout/bug1/bug1.go
@@ -1,11 +1,16 @@
 package bug1
 
+import "sync"
+
 // Counter stores a count.
 type Counter struct {
-	n int64
+	mu sync.Mutex
+	n  int64
 }
 
 // Inc increments the count in the Counter.
 func (c *Counter) Inc() {
+	c.mu.Lock()
 	c.n++
+	c.mu.Unlock()
 }
